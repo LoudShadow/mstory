b2bc69ca7f4efeabb714511721c470d21aa66100
testName <email@example.com>
2023-10-17 13:57:25 +0100

    Adding in more pages

--- src/main/java/com/group1/mstory/controller/BookController.java
+++ src/main/java/com/group1/mstory/controller/BookController.java
@@ -1,19 +1,23 @@
 package com.group1.mstory.controller;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.stereotype.Component;
 import com.group1.mstory.model.BookTile;
+import com.mysql.cj.x.protobuf.MysqlxPrepare.Prepare;
 import com.group1.mstory.connectors.JdbcConnector;
 
 
 @Component
 public class BookController {
 
+
     public static ArrayList<BookTile> getAllBookTiles(){
         JdbcConnector jdbcConnector = new JdbcConnector("jdbc:mysql://192.168.1.106:3310/MStorey","root","password");
+
         String sql = "SELECT Books.*, GROUP_CONCAT(Authors.Name) AS Authors FROM Books \r\n" + //
                 "INNER JOIN Author_Book ON Author_Book.BookId = Books.BooksId \r\n" + //
                 "INNER JOIN Authors ON Authors.AuthorId = Author_Book.AuthorId\r\n" + //
@@ -25,15 +29,7 @@ public class BookController {
             ResultSet rs = jdbcConnector.prepareAndExecuteStatement(sql);
 
             while (rs.next()){
-                BookTile bt = new BookTile();
-                bt.setTitle(rs.getString("title"));
-                bt.setIsbn(rs.getString("isbn"));
-                bt.setImage(rs.getString("imageurl"));
-                bt.setDescription(rs.getString("description"));
-                bt.setPublisherId(rs.getInt("publisherid"));
-                bt.setAuthors( new ArrayList<String>(Arrays.asList(rs.getString("authors").split(","))));
-
-
+                BookTile bt = new BookTile(rs);
                 bookTileList.add(bt);
             }
 
@@ -46,6 +42,41 @@ public class BookController {
 
         return bookTileList;
     }
+
+
+    private static BookTile getBook(String sql, int id){
+        JdbcConnector jdbcConnector = new JdbcConnector("jdbc:mysql://192.168.1.106:3310/MStorey","root","password");
+
+        try {
+            PreparedStatement ps = jdbcConnector.prepareStatement(sql);
+            ps.setInt(1, id);
+            System.out.println(ps.toString());
+            ResultSet rs = ps.executeQuery();
+
+            return new BookTile(rs);
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static BookTile getBookByProductId(int id){
+        String sql = "SELECT Books.* FROM Books" + 
+        "INNER JOIN Book_Product ON Book_Product.BookId = Books.BooksId " +
+        "INNER JOIN Products ON Products.BookId = Book_Product.ProductId" + 
+        "WHERE Products.ProductId = ?;";
+
+        return getBook(sql, id);
+    }
+
+    public static BookTile getBookByBookId(int id){
+        String sql = "SELECT * FROM Books WHERE BooksId = ?;";
+        return getBook(sql, id);
+    }
+
+
+    
 
     
 }
--- src/main/java/com/group1/mstory/view/CorePages.java
+++ src/main/java/com/group1/mstory/view/CorePages.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.group1.mstory.controller.BookController;
 import com.group1.mstory.model.BookTile;
@@ -14,27 +15,32 @@ import com.group1.mstory.model.BookTile;
 public class CorePages {
     @RequestMapping(value = "/",method = RequestMethod.GET)
     public String index(Model model){
-        ArrayList<String> authors = new ArrayList<String>();
-        authors.add("J.R.R. Tolkien");
-        ArrayList<BookTile> books = new ArrayList<BookTile>();
-        books.add(new BookTile("The Hobbit", authors, "A book about a hobbit", 1, "1234567890", 10, "https://upload.wikimedia.org/wikipedia/en/4/4a/TheHobbit_FirstEdition.jpg"));
-        books.add(new BookTile("The Hobbit", authors, "A book about a hobbit", 1, "1234567890", 10, "https://upload.wikimedia.org/wikipedia/en/4/4a/TheHobbit_FirstEdition.jpg"));
+        // ArrayList<String> authors = new ArrayList<String>();
+        // authors.add("J.R.R. Tolkien");
+        // ArrayList<BookTile> books = new ArrayList<BookTile>();
+        // books.add(new BookTile("The Hobbit", authors, "A book about a hobbit", 1, "1234567890", 10, "https://upload.wikimedia.org/wikipedia/en/4/4a/TheHobbit_FirstEdition.jpg"));
+        // books.add(new BookTile("The Hobbit", authors, "A book about a hobbit", 1, "1234567890", 10, "https://upload.wikimedia.org/wikipedia/en/4/4a/TheHobbit_FirstEdition.jpg"));
 
+
+        ArrayList<BookTile> books = BookController.getAllBookTiles();
         model.addAttribute("books", books);
 
         return "allBooks.html";
     }
+
     @RequestMapping(value = "/bookpage",method = RequestMethod.GET)
-    public String page(){
+    public String page(@RequestParam("id") String idParam){
+        int id = Integer.parseInt(idParam);
+
+
+        BookTile bt = BookController.getBookByBookId(id);
+        System.out.println(bt.getTitle());
+
         return "bookpage.html";
     }
 
     @RequestMapping(value = "/testBookPage", method = RequestMethod.GET)
     public String testBookPage(){
-        ArrayList<BookTile> bookTiles = BookController.getAllBookTiles();
-
-        
-
         return "bookpage.html";
     }
 }
