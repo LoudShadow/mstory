d3f598551ef993a231f37ef56c1feb4c59d6a4b9
loudshadow <josephmevans2002@gmail.com>
2023-10-17 15:38:08 +0100

    added post links to the frontend

--- src/main/java/com/group1/mstory/view/CorePages.java
+++ src/main/java/com/group1/mstory/view/CorePages.java
@@ -18,13 +18,6 @@ import com.group1.mstory.model.BookTile;
 public class CorePages {
     @RequestMapping(value = "/",method = RequestMethod.GET)
     public String index(Model model){
-        // ArrayList<String> authors = new ArrayList<String>();
-        // authors.add("J.R.R. Tolkien");
-        // ArrayList<BookTile> books = new ArrayList<BookTile>();
-        // books.add(new BookTile("The Hobbit", authors, "A book about a hobbit", 1, "1234567890", 10, "https://upload.wikimedia.org/wikipedia/en/4/4a/TheHobbit_FirstEdition.jpg"));
-        // books.add(new BookTile("The Hobbit", authors, "A book about a hobbit", 1, "1234567890", 10, "https://upload.wikimedia.org/wikipedia/en/4/4a/TheHobbit_FirstEdition.jpg"));
-
-
         ArrayList<BookTile> books = BookController.getAllBookTiles();
         model.addAttribute("books", books);
 
@@ -32,11 +25,12 @@ public class CorePages {
     }
 
     @RequestMapping(value = "/bookpage",method = RequestMethod.GET)
-    public String page(@RequestParam("id") String idParam){
+    public String page(Model model,@RequestParam("id") String idParam){
         int id = Integer.parseInt(idParam);
 
 
         BookTile bt = BookController.getBookByBookId(id);
+        model.addAttribute("book", bt);
         System.out.println(bt.getTitle());
 
         return "bookpage.html";
