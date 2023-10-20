23ad1135d4239acdb8f025d3be5fa4c63182e1ca
Danvs60 <daniel.bartolini@outlook.com>
2023-10-20 10:10:37 +0100

    disabled csrf

--- src/main/java/com/group1/mstory/login/WebSecurityConfig.java
+++ src/main/java/com/group1/mstory/login/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -34,7 +35,7 @@ public class WebSecurityConfig {
 
         @Bean
         public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-                http
+                http.csrf(AbstractHttpConfigurer::disable)
                                 .authorizeHttpRequests((requests) -> requests
                                                 .anyRequest()
                                                 .authenticated())
